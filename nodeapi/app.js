4fabcaf86b69c3240012871b8e02a5a8703fc68f
SandroSD <sdezerio@soulitsolutions.com>
2020-03-30 16:11:00 -0300

    Continuar desde el video 31

---
+++ nodeapi/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+	.then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => {
+	console.log(`DB connection error: ${err.message}`);
+});
+
+const postRoutes = require('./routes/post');
+
+app.use(morgan('dev'));
+
+app.use("/", postRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {console.log(`NodeJS api is listening to port ${port}`)});
